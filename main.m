b22ecb49fabe7258a02e9feabe4423dda929986e
aksland2 <aksland2@mechse-poets-49.ad.uillinois.edu>
2020-12-11 10:51:07 -0600

    Added SUPPORT_FILES folder and ToySystem data to supply connection sets.

--- main.m
+++ main.m
@@ -2,20 +2,25 @@ clear all
 close all
 clc
 
-tank1 = Tank('Name','Tank 1','T_init',50);
-load1 = HeatLoad('Name','Load 1','T_init',16);
+%% Load Toy System Connection Set
+load ToySystem
+figure; plot(G)
+
+%% Generate and plot component models
 HX1   = HeatExchanger('Name','HX 1','T1_init',16);
-SJ1   = SplitJunction('Name','J3S2','n_in',3,'n_out',2);
+load1 = HeatLoad('Name','Load 1','T_init',16);
+SJ1   = SplitJunction('Name','J2S1','n_in',2,'n_out',1);
+SJ2   = SplitJunction('Name','J1S2','n_in',1,'n_out',2);
+tank1 = Tank('Name','Tank 1','T_init',50);
+tank2 = Tank('Name','Tank 1','T_init',50);
 
-Comps = [tank1; load1; HX1; SJ1];
+% don't change the order of the components
+Comps = [HX1; load1; SJ1; SJ2; tank1; tank2];
 
-
+% plot component graphs
 figure
 for i = 1:length(Comps)
-    subplot(2,2,i)
+    subplot(3,2,i)
     plot(Comps(i).Model)
     title(Comps(i).Name)
 end
-
-
-Graphs = [Comps(:).Model];
