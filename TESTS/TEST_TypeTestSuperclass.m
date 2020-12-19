2ce3d097511900d452d247df8936dab8f8690d00
renkert2 <renkert2@illinous.edu>
2020-12-19 09:55:12 -0600

    Updated Type_Super and Type_Capacitance

---
+++ TESTS/TEST_TypeTestSuperclass.m
@@ -0,0 +1,14 @@
+% t_cap = Type_Capacitance('a^2') % - Returns invalid type definition message
+
+% syms a
+% t_cap = Type_Capacitance(a^2) % - Returns invalid type definition message
+
+t_cap = Type_Capacitance('x^2'); % - success
+
+syms x
+t_cap = Type_Capacitance(x^2); % - success
+
+t_cap.Val_Str = "x^3";
+
+t_cap.Val_Sym = x^5;
+
