b9bf85fc3ebdd5f27c6fd51c24e273fb24158c03
renkert2 <renkert2@illinous.edu>
2020-12-29 09:46:14 -0600

    Added GraphInput to component class definitions

--- COMPONENTS/HeatExchanger.m
+++ COMPONENTS/HeatExchanger.m
@@ -61,11 +61,15 @@ classdef HeatExchanger < Component
             Vertex(5) = GraphVertex_External('Description','Outlet S1','Capacitance',C(1));
             Vertex(6) = GraphVertex_External('Description','Outlet S2','Capacitance',C(1));
             
+            % Define Inputs
+            I(1) = GraphInput('HX Input 1');
+            I(2) = GraphInput('HX Input 2');
+            
             % Define Edges
-            Edge(1) = GraphEdge_Internal('PowerFlow',P(1),'Input',1,'Port',1,'Coefficient',obj.cp_f1,'TailVertex',Vertex(E(1,1)),'HeadVertex',Vertex(E(1,2)));
-            Edge(2) = GraphEdge_Internal('PowerFlow',P(1),'Input',1,'Port',2,'Coefficient',obj.cp_f1,'TailVertex',Vertex(E(2,1)),'HeadVertex',Vertex(E(2,2)));
-            Edge(3) = GraphEdge_Internal('PowerFlow',P(1),'Input',2,'Port',3,'Coefficient',obj.cp_f2,'TailVertex',Vertex(E(3,1)),'HeadVertex',Vertex(E(3,2)));
-            Edge(4) = GraphEdge_Internal('PowerFlow',P(1),'Input',2,'Port',4,'Coefficient',obj.cp_f2,'TailVertex',Vertex(E(4,1)),'HeadVertex',Vertex(E(4,2)));
+            Edge(1) = GraphEdge_Internal('PowerFlow',P(1),'Input',I(1),'Port',1,'Coefficient',obj.cp_f1,'TailVertex',Vertex(E(1,1)),'HeadVertex',Vertex(E(1,2)));
+            Edge(2) = GraphEdge_Internal('PowerFlow',P(1),'Input',I(1),'Port',2,'Coefficient',obj.cp_f1,'TailVertex',Vertex(E(2,1)),'HeadVertex',Vertex(E(2,2)));
+            Edge(3) = GraphEdge_Internal('PowerFlow',P(1),'Input',I(2),'Port',3,'Coefficient',obj.cp_f2,'TailVertex',Vertex(E(3,1)),'HeadVertex',Vertex(E(3,2)));
+            Edge(4) = GraphEdge_Internal('PowerFlow',P(1),'Input',I(2),'Port',4,'Coefficient',obj.cp_f2,'TailVertex',Vertex(E(4,1)),'HeadVertex',Vertex(E(4,2)));
             Edge(5) = GraphEdge_Internal('PowerFlow',[P(2); P(3)],'Coefficient',[obj.HTC -obj.HTC],'TailVertex',Vertex(E(5,1)),'HeadVertex',Vertex(E(5,2)));
             
             g = Graph(Vertex,Edge);
