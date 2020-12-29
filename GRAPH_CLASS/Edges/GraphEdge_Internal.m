566ff7f43a37d0aafd94a9664503548c5abab253
renkert2 <renkert2@illinous.edu>
2020-12-29 09:41:21 -0600

    Modified GraphEdge_Internal to ensure all property vectors are column vectors

--- GRAPH_CLASS/Edges/GraphEdge_Internal.m
+++ GRAPH_CLASS/Edges/GraphEdge_Internal.m
@@ -5,8 +5,8 @@ classdef GraphEdge_Internal < GraphEdge
         PowerFlow Type_PowerFlow = Type_PowerFlow.empty()
         Input GraphInput = GraphInput.empty()
         Port
-        Coefficient double = 0
-        TailVertex GraphVertex = GraphVertex()
+        Coefficient (:,1) double = 0
+        TailVertex (1,1) GraphVertex = GraphVertex()
 
     end
     
@@ -18,6 +18,36 @@ classdef GraphEdge_Internal < GraphEdge
         function set.TailVertex(obj, tail_vertex)
             assert(numel(tail_vertex)<=1, 'Edge can have only one tail vertex');
             obj.TailVertex = tail_vertex;
+        end
+        
+                
+        function set.PowerFlow(obj, power_flow) % Ensure PowerFlows are column vector
+            if ~iscolumn(power_flow)
+                power_flow = power_flow';
+            end
+            obj.PowerFlow = power_flow;
+        end
+        
+        function set.Input(obj, input) % Ensure inputs are column vector
+            if ~iscolumn(input)
+                input = input';
+            end
+            obj.Input = input;
+        end
+                 
+        function set.Port(obj, port) % Ensure ports are column vector
+            if ~iscolumn(port)
+                port = port';
+            end
+            obj.Port = port;
+        end
+        
+                
+        function set.Coefficient(obj, coeff) % Ensure coefficients are column vector
+            if ~iscolumn(coeff)
+                coeff = coeff';
+            end
+            obj.Coefficient = coeff;
         end
     end
 end
