9f23d37a4583339da592aa5e6d1142abc6b2c076
renkert2 <renkert2@illinous.edu>
2020-12-31 13:10:56 -0600

    Created TEST file for GenSysGraph_Phil

---
+++ TESTS/TEST_GenSysGraph_Phil.m
@@ -0,0 +1,32 @@
+clear all
+close all
+clc
+
+
+%% Load Toy System Connection Set
+load ToySystem
+% figure; plot(G)
+
+%% Generate and plot component models
+
+HX1   = HeatExchanger('Name','HX 1','T1_init',16);
+load1 = HeatLoad('Name','Load 1','T_init',16);
+SJ1   = SplitJunction('Name','J2S1','n_in',2,'n_out',1);
+SJ2   = SplitJunction('Name','J1S2','n_in',1,'n_out',2);
+tank1 = Tank('Name','Tank 1','T_init',50);
+tank2 = Tank('Name','Tank 2','T_init',50);
+
+
+% don't change the order of the components
+Comps = [HX1; load1; SJ1; SJ2; tank1; tank2];
+Graphs = [Comps(:).graph];
+
+%%
+Sys_graph = GenSysGraph_Phil(Graphs,ConnectE,ConnectV);
+figure
+plot(Sys_graph,'NodeColor','r','EdgeColor','b')
+
+Sys = GraphModel(Sys_graph);
+
+
+
