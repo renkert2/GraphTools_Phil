6c2c7d47ce0793a8421a796fc66436718a65b8df
renkert2 <renkert2@illinous.edu>
2021-01-18 08:33:20 -0600

    Added isCompatible function to Domains

--- GRAPH_CLASS/Vertices/Domains.m
+++ GRAPH_CLASS/Vertices/Domains.m
@@ -9,4 +9,23 @@ classdef Domains
         MechanicalTranslational
         Thermal
     end
+    
+    methods
+        function x = isCompatible(varargin)
+            if nargin > 1
+                obj_array = [varargin{:}];
+            else
+                obj_array = varargin{1};
+            end
+            
+            assert(numel(obj_array)>=2,'Array of two or more Domain objects required');
+            
+            definedDomains = obj_array(Domains.Abstract ~= obj_array); % Allow combination of abstract domains
+            if ~isempty(definedDomains)
+                x = all(definedDomains(1) == definedDomains);
+            else
+                x = true;
+            end
+        end
+    end
 end
