005c414ad612547a1616dbdf0d3550c4a380655f
renkert2 <renkert2@illinous.edu>
2021-01-18 15:45:27 -0600

    Suppressed tail and head vertex connection warning

--- GRAPH_CLASS/Edges/GraphEdge_Internal.m
+++ GRAPH_CLASS/Edges/GraphEdge_Internal.m
@@ -15,26 +15,26 @@ classdef GraphEdge_Internal < GraphEdge
             obj@GraphEdge(varargin{:}); % calls the superclass constructor
         end
         
-%         function set.TailVertex(obj, tail_vertex)
-%             assert(numel(tail_vertex)<=1, 'Edge can have only one tail vertex');
+        function set.TailVertex(obj, tail_vertex)
+            assert(numel(tail_vertex)<=1, 'Edge can have only one tail vertex');
 %             if ~isempty(obj.HeadVertex)
 %                 if ~isConnectable(obj.HeadVertex.VertexType, tail_vertex.VertexType)
 %                     warning('Tail vertex is not connectable with existing head vertex')
 %                 end
 %             end
-%             obj.TailVertex = tail_vertex;
-%         end
+            obj.TailVertex = tail_vertex;
+        end
         
-%         function set.HeadVertex(obj, head_vertex) % Overrides set.HeadVertex of GraphEdge
-%             assert(numel(head_vertex)<=1, 'Edge can have only one head vertex');
+        function set.HeadVertex(obj, head_vertex) % Overrides set.HeadVertex of GraphEdge
+            assert(numel(head_vertex)<=1, 'Edge can have only one head vertex');
 %             if ~isempty(obj.TailVertex)
 %                 if ~isConnectable(obj.TailVertex.VertexType, head_vertex.VertexType)
 %                     warning('Head vertex is not connectable with existing tail vertex')
 %                 end
 %             end
-%             obj.HeadVertex = head_vertex;
-%         end
-%         
+            obj.HeadVertex = head_vertex;
+        end
+        
         function set.PowerFlow(obj, power_flow) % Ensure PowerFlows are column vector
             if ~iscolumn(power_flow)
                 power_flow = power_flow';
