e1f561885453e0b9a403b2c952b99c561d3d6e1a
Renkert <renkert2@illinois.edu>
2021-05-14 11:25:35 -0500

    Allowed ComponentData to read Excel files formatted in a certain way and added a few features to compParamValue

--- GRAPH_CLASS/ComponentData.m
+++ GRAPH_CLASS/ComponentData.m
@@ -7,7 +7,7 @@ classdef ComponentData
         Component string % Corresponds to Component class name
         Make string
         Model string
-        SerialNumber string
+        SKU string
         Description string
         
         Data (:,1) compParamValue
@@ -15,13 +15,105 @@ classdef ComponentData
     
     methods
         function obj = ComponentData(comp, make, model, data)
-            obj.Component = comp;
-            obj.Make = make;
-            obj.Model = model;
-            obj.Data = data(:);
+            if nargin > 0
+                obj.Component = comp;
+                obj.Make = make;
+                obj.Model = model;
+                obj.Data = data(:);
+
+                for i = 1:numel(obj.Data)
+                    obj.Data(i).Component = obj.Component;
+                end
+            end
+        end
+        
+        function [tbl,param_table] = table(obj_array)
+            pdat = obj_array(1).Data;
+            param_table = table(pdat);
+            pvars = string(param_table.Properties.VariableNames);
+            pvi = pvars ~= "Value";
+            param_table = param_table(:,pvi);
             
-            for i = 1:numel(obj.Data)
-                obj.Data(i).Component = obj.Component;
+            params = [pdat.Sym];
+            comp_fields = ["Make", "Model", "SKU", "Description"];
+            varnames = [comp_fields, params];
+            vartypes = [repmat("string",1,4) repmat("double",1,numel(params))];
+            tbl = table('Size', [numel(obj_array), numel(varnames)], 'VariableTypes', vartypes, 'VariableNames', varnames);
+            for i = 1:numel(obj_array)
+                for f = comp_fields
+                    tbl(i,:).(f) = obj_array(i).(f);
+                end
+                for j = 1:numel(params)
+                    f = params(j);
+                    tbl(i,:).(f) = obj_array(i).Data(j).Value;
+                end
+            end
+        end
+    end
+    
+    methods (Static)
+        function obj_array = importFromFile(file)
+            arguments
+                file string
+            end
+            
+            data = readcell(file, 'TextType', 'string', 'MissingRule', 'fill');
+            
+            % Breakpoints: Component ; empty ; Parameter Data; empty ; Component Data
+            first_col = data(:,1);
+            [ir_comp,ic_comp] = find(cellfun(@(x) matchText(x,"Component"), data));
+            [ir_param, ~] = find(cellfun(@(x) matchText(x,"Parameter"), data));
+            [ir_data, ic_data] = find(cellfun(@(x) matchText(x,"Data"), data));
+            
+            % Read Component
+            component = data{ir_comp,ic_comp+1};
+            
+            % Read Parameters
+            param_props = [first_col{ir_param:ir_data-1}];
+            param_data = data(ir_param:ir_data-1,2:end);
+            missing_cols = all(cellfun(@ismissing, param_data),1);
+            param_data = param_data(:,~missing_cols)';
+            param_table = cell2table(param_data, 'VariableNames', param_props);
+            params = param_table{:,1}';
+            
+            % Component Properties
+            comp_props = rmmissing([data{ir_data+1,ic_data:end}]);
+            
+            % Get Data
+            varnames = [comp_props params];
+            data_table = cell2table(data(ir_data+2:end,:),'VariableNames',varnames);
+            
+            % Make Template for compParamValue array
+            data_template = compParamValue.importFromTable(param_table);
+            
+            % Create ComponentData Array
+            N = height(data_table);
+            obj_array = ComponentData.empty(N,0);
+            
+            for i = 1:N
+                cd = ComponentData();
+                cd.Component = component;
+                for j = 1:numel(comp_props)
+                    field = comp_props(j);
+                    cd.(field) = data_table(i,:).(field);
+                end
+                
+                cpvdata = data_template;
+                for j = 1:numel(params)
+                    cpvdata(j).Value = data_table(i,:).(params(j));
+                    cpvdata(j).Component = component;
+                end
+                cd.Data = cpvdata;
+                obj_array(i,1) = cd;
+            end
+            
+            % Helper Functions
+            function l = matchText(str,patt)
+                if isa(str, 'string') || isa(str, 'char')
+                    l = contains(str,patt, 'IgnoreCase', true);
+                else
+                    l = false;
+                end
             end
         end
     end
--- GRAPH_CLASS/compParamValue.m
+++ GRAPH_CLASS/compParamValue.m
@@ -5,7 +5,7 @@ classdef compParamValue
     properties
         Sym string
         Value double
-        Unit 
+        Unit
         Description string
         
         Component string % Name of component object corresponding to this parameter
@@ -13,20 +13,41 @@ classdef compParamValue
     end
     
     methods
-        function obj = compParamValue(sym, val, opts)
-            arguments
-                sym 
-                val
-                opts.Unit = ""
-                opts.Description = ""
-                opts.Component = ""
+        function obj = compParamValue(sym, varargin)
+            if nargin == 1
+                obj.Sym = sym;
+            elseif nargin > 1
+                obj.Sym = sym;
+                obj = my_inputparser(obj, varargin{:});
             end
+        end
+        
+        function t = table(obj_array)
+            w=warning('off','MATLAB:structOnObject');
+            t=struct2table(arrayfun(@struct, obj_array));
+            warning(w);
+        end
+    end
+    
+    methods (Static)
+        function obj_array = importFromTable(param_table)
+            % Construct array of compParamValues from table, whos
+            % VariableNames correspond to compParamValue properties.
+            % First column must be Sym
             
-            obj.Sym = sym;
-            obj.Value = val;
-            obj.Unit = opts.Unit;
-            obj.Description = opts.Description;
-            obj.Component = opts.Component;
+            N = height(param_table);
+            propnames = string(param_table.Properties.VariableNames(2:end));
+            syms = param_table{:,1};
+            
+            obj_array = compParamValue.empty(N,0);
+            for i = 1:N
+                obj = compParamValue(syms(i));
+                for j = 1:numel(propnames)
+                    field = propnames(j);
+                    obj.(field) = param_table(i,:).(field);
+                end
+                obj_array(i,1) = obj;
+            end
         end
     end
 end
